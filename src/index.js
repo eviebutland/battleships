1e4d6ce05e603c8a1a8a9b2512174fe33cd248df
Evie <evie.butland@gmail.com>
2019-10-05 10:18:19 +0700

    making clicks appear

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-document.get
+window.onload=function(){
+    console.log('loaded');
+
+
+    // limit clicks 
+    let bombs = 30;
+    
+    // hit ship 
+    $('#water td').on('click', function() {
+        $(this).addClass("hit");
+    })
+
+
+
+
+    $('#restart').on('click', function (){
+        console.log('restarted')
+    })
+
+}
