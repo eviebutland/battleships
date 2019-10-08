b4403d2460be1b4415bec172468b1ac54fb26414
Evie <evie.butland@gmail.com>
2019-10-08 12:32:13 +0700

    creating hit and miss counter

--- src/index.js
+++ src/index.js
@@ -4,22 +4,30 @@ window.onload=function(){
     // limit clicks 
     let bombs = 30;
     
+    let hits = 0;
+    let misses = 0;
     // hit ship 
     if (bombs >= 1){
         $('#water td').on('click', function() {
             $(this).addClass("hit");
+            hits++
+            $('#no-hits').text(`Hits: ${hits}`);
             console.log($('td').index(this)); // index of the box
             bombs--;
             $('h2').text(`No. Bombs remaining: ${bombs}`)
         })
+
+        
+        $('#no-misses').text(`Misses: ${misses}`);
+
     } else {
         bombs = 0;
     }
 
     // miss ship 
-    $('#water td').on('click', function(){
-        $(this).addClass("miss");
-    })
+    // $('#water td').on('click', function(){
+    //     $(this).addClass("miss");
+    // })
 
 
     $('#restart').on('click', function (){
