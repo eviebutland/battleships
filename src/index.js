90ad0a00b8ea19948d947c53910dc1e69f38bed3
Evie <evie.butland@gmail.com>
2019-10-14 11:27:29 +0700

    making styling changes and functions

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 window.onload=function(){
-    console.log('loaded');
-
-    // limit clicks 
     let bombs = 30;
-    
     let hits = 0;
     let misses = 0;
+
+    function setText(hits, misses, bombs){
+        $('#no-hits').text(`Hits: ${hits}`);
+        $('#no-misses').text(`Misses: ${misses}`); 
+        $('h2').text(`No. Bombs remaining: ${bombs}`);
+    }
+
+    
+
+
     // hit ship 
     if (bombs >= 1){
         $('#water td').on('click', function() {
             $(this).addClass("hit");
             hits++
-            $('#no-hits').text(`Hits: ${hits}`);
             console.log($('td').index(this)); // index of the box
             bombs--;
-            $('h2').text(`No. Bombs remaining: ${bombs}`)
-        })
-
-        
-        $('#no-misses').text(`Misses: ${misses}`);
-
+            setText(hits, misses, bombs);
+        })  
     } else {
         bombs = 0;
     }
 
-    // miss ship 
-    // $('#water td').on('click', function(){
-    //     $(this).addClass("miss");
-    // })
-
-
     $('#restart').on('click', function (){
         console.log('restarted');
         $('#water td').addClass('restart');
+        hits = 0;
+        misses = 0;
+        bombs = 30;
+        setText(hits, misses, bombs);
     })
 
 }
